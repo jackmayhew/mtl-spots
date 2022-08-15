fdc18f97d3f101af34aad2803d12d1950e33a501
jack <jacksonmayhew9@gmail.com>
2022-08-14 19:26:28 -0700

    api endpoints, hosting on heroku

--- utils/domain.js
+++ utils/domain.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://mtl-spots.vercel.app/'
+export const server = dev ? 'http://localhost:3000' : 'https://morning-wave-33762.herokuapp.com'
