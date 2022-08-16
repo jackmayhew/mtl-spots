545480f683a77360cc93d61b48f785c7f32b877f
jack <jacksonmayhew9@gmail.com>
2022-08-16 16:15:13 -0700

    updating head and meta tags

--- components/Page/Meta.tsx
+++ components/Page/Meta.tsx
@@ -4,9 +4,8 @@ function Meta({ title, keywords, description }) {
   return (
     <Head>
       <title>{title}</title>
-      <html lang="en" />
       <meta charSet="UTF-8" />
-      <meta name="description" content={description} />
+      <meta name="description" content="Find And Share Skate Spots In Montreal" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="keywords" content={keywords} />
       <meta name="language" content="Spanish" />
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false,
-  reactStrictMode: true
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US' 
+  }
 }
 
 module.exports = nextConfig
