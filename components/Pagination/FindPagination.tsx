89188f3db7455a23c62ed41c41ce4e3919180274
jack <jacksonmayhew9@gmail.com>
2022-09-10 16:17:40 -0700

    smooth scroll support for safari

--- components/Pagination/FindPagination.tsx
+++ components/Pagination/FindPagination.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import smoothscroll from 'smoothscroll-polyfill';
 
 function Pagination({ count, page, spotCategory }) {
+  
+  smoothscroll.polyfill();
+
+
   let pageIndex = parseInt(page);
   page === undefined ? (pageIndex = 1) : (pageIndex = parseInt(page));
 
