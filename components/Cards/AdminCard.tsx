161066a41faed50b497ae7f967a730d22ab8b124
jack <jacksonmayhew9@gmail.com>
2022-09-24 16:14:19 -0700

    spots are able to be deleted from admin page

--- components/Cards/AdminCard.tsx
+++ components/Cards/AdminCard.tsx
@@ -24,6 +24,7 @@ function AdminCard({ spot }) {
                     "Content-Type": "application/json",
                 },
             });
+            router.reload()
         } catch (error) {
             console.log(error);
         }
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -7,9 +7,14 @@ import { useUser } from "@auth0/nextjs-auth0";
 import Link from "next/link";
 import Pagination from "../components/Pagination/Pagination";
 import AdminCard from "../components/Cards/AdminCard";
-SingleSpot.title = "admin"
+SingleSpot.title = "admin";
 
-function SingleSpot({ initialSpots, initialCount, initialPage, initialCategory }) {
+function SingleSpot({
+  initialSpots,
+  initialCount,
+  initialPage,
+  initialCategory,
+}) {
   const menuRef = useRef(null);
   const [listening, setListening] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
@@ -20,168 +25,173 @@ function SingleSpot({ initialSpots, initialCount, initialPage, initialCategory }
   const { user } = useUser();
 
 
-
   return (
-    <div className="outer__inner">
-      <div className="section-mb80 main main_cars-category">
-        <div className="section catalog">
-          <div className="sorting">
-            <div className="sorting__center center">
-              <div className="control">
-                <ul className="breadcrumbs">
-                  <li className="breadcrumbs__item">
-                    <Link href={`/`}>
-                      <a className="breadcrumbs__link">Home</a>
-                    </Link>
-                  </li>
-                  <li className="breadcrumbs__item">
-                    <Link href={`/spots`}>
-                      <a className="breadcrumbs__link">Spots</a>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="sorting__body">
-                <div className="sorting__box">
-                  <h2 className="sorting__title h2">
-                    {initialCount} Spots Found
-                  </h2>
-                  <div className="sorting__line">
-                    <div className="sorting__details">
-                      {query === "All" || !query
-                        ? "Showing All Spots"
-                        : `Showing ${query}`}
+    <div>
+      {user && user.sub === process.env.NEXT_PUBLIC_ADMIN_ID ? (
+        <div className="outer__inner">
+          <div className="section-mb80 main main_cars-category">
+            <div className="section catalog">
+              <div className="sorting">
+                <div className="sorting__center center">
+                  <div className="control">
+                    <ul className="breadcrumbs">
+                      <li className="breadcrumbs__item">
+                        <Link href={`/`}>
+                          <a className="breadcrumbs__link">Home</a>
+                        </Link>
+                      </li>
+                      <li className="breadcrumbs__item">
+                        <Link href={`/spots`}>
+                          <a className="breadcrumbs__link">Spots</a>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="sorting__body">
+                    <div className="sorting__box">
+                      <h2 className="sorting__title h2">
+                        {initialCount} Spots Found
+                      </h2>
+                      <div className="sorting__line">
+                        <div className="sorting__details">
+                          {query === "All" || !query
+                            ? "Showing All Spots"
+                            : `Showing ${query}`}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="sorting__foot spotspage__search">
+                    <div className="sorting__select" ref={menuRef}>
+                      <div
+                        className={
+                          isOpen
+                            ? "nice-select select open"
+                            : "nice-select select"
+                        }
+                        onClick={toggle}
+                      >
+                        <span className="current">
+                          {query ? query : "Browse By"}
+                        </span>
+                        <ul className="list">
+                          <Link scroll={false} href={`/admin?category=`}>
+                            <li
+                              onClick={() => setQuery("All")}
+                              className={
+                                query === "All"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>All</a>
+                            </li>
+                          </Link>
+                          <Link scroll={false} href={`/admin?category=stairs`}>
+                            <li
+                              onClick={() => setQuery("stairs")}
+                              className={
+                                query === "stairs"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>Stairs</a>
+                            </li>
+                          </Link>
+                          <Link scroll={false} href={`/admin?category=rails`}>
+                            <li
+                              onClick={() => setQuery("rails")}
+                              className={
+                                query === "rails"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>Rails</a>
+                            </li>
+                          </Link>
+                          <Link scroll={false} href={`/admin?category=ledges`}>
+                            <li
+                              onClick={() => setQuery("ledges")}
+                              className={
+                                query === "ledges"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>Ledges</a>
+                            </li>
+                          </Link>
+                          <Link scroll={false} href={`/admin?category=gaps`}>
+                            <li
+                              onClick={() => setQuery("gaps")}
+                              className={
+                                query === "gaps"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>Gaps</a>
+                            </li>
+                          </Link>
+                          <Link
+                            scroll={false}
+                            href={`/admin?category=skate parks`}
+                          >
+                            <li
+                              onClick={() => setQuery("skate parks")}
+                              className={
+                                query === "skate parks"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>skate parks</a>
+                            </li>
+                          </Link>
+                          <Link scroll={false} href={`/admin?category=other`}>
+                            <li
+                              onClick={() => setQuery("other")}
+                              className={
+                                query === "other"
+                                  ? "option selected focus"
+                                  : "option"
+                              }
+                            >
+                              <a>Other</a>
+                            </li>
+                          </Link>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="sorting__foot spotspage__search">
-                <div className="sorting__select" ref={menuRef}>
-                  <div
-                    className={
-                      isOpen ? "nice-select select open" : "nice-select select"
-                    }
-                    onClick={toggle}
-                  >
-                    <span className="current">
-                      {query ? query : "Browse By"}
-                    </span>
-                    <ul className="list">
-                      <Link scroll={false} href={`/spots?category=`}>
-                        <li
-                          onClick={() => setQuery("All")}
-                          className={
-                            query === "All" ? "option selected focus" : "option"
-                          }
-                        >
-                          <a>All</a>
-                        </li>
-                      </Link>
-                      <Link scroll={false} href={`/spots?category=stairs`}>
-                        <li
-                          onClick={() => setQuery("stairs")}
-                          className={
-                            query === "stairs"
-                              ? "option selected focus"
-                              : "option"
-                          }
-                        >
-                          <a>Stairs</a>
-                        </li>
-                      </Link>
-                      <Link scroll={false} href={`/spots?category=rails`}>
-                        <li
-                          onClick={() => setQuery("rails")}
-                          className={
-                            query === "rails"
-                              ? "option selected focus"
-                              : "option"
-                          }
-                        >
-                          <a>Rails</a>
-                        </li>
-                      </Link>
-                      <Link scroll={false} href={`/spots?category=ledges`}>
-                        <li
-                          onClick={() => setQuery("ledges")}
-                          className={
-                            query === "ledges"
-                              ? "option selected focus"
-                              : "option"
-                          }
-                        >
-                          <a>Ledges</a>
-                        </li>
-                      </Link>
-                      <Link scroll={false} href={`/spots?category=gaps`}>
-                        <li
-                          onClick={() => setQuery("gaps")}
-                          className={
-                            query === "gaps"
-                              ? "option selected focus"
-                              : "option"
-                          }
-                        >
-                          <a>Gaps</a>
-                        </li>
-                      </Link>
-                      <Link scroll={false} href={`/spots?category=skate parks`}>
-                        <li
-                          onClick={() => setQuery("skate parks")}
-                          className={
-                            query === "skate parks"
-                              ? "option selected focus"
-                              : "option"
-                          }
-                        >
-                          <a>skate parks</a>
-                        </li>
-                      </Link>
-                      <Link scroll={false} href={`/spots?category=other`}>
-                        <li
-                          onClick={() => setQuery("other")}
-                          className={
-                            query === "other"
-                              ? "option selected focus"
-                              : "option"
-                          }
-                        >
-                          <a>Other</a>
-                        </li>
-                      </Link>
-                    </ul>
+
+              <div className="catalog__body">
+                <div className="catalog__center center">
+                  <div className="catalog__cards">
+                    {initialSpots.map((spot) => (
+                      <AdminCard spot={spot} key={spot._id} />
+                    ))}
                   </div>
+
+                  {initialSpots.length > 0 && (
+                    <div className="catalog__btns">
+                      <Pagination
+                        count={initialCount}
+                        page={initialPage}
+                        url={`/admin?category=${initialCategory}&`}
+                      />
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-          </div>
-
-
-          {(user && user.sub === process.env.NEXT_PUBLIC_ADMIN_ID ) && (
-                <h1>{user.sub}</h1>
-           )}
-
-          <div className="catalog__body">
-            <div className="catalog__center center">
-              <div className="catalog__cards">
-                {initialSpots.map((spot) => (
-                  <AdminCard spot={spot} key={spot._id} />
-                ))}
-              </div>
-
-              {initialSpots.length > 0 && (
-                <div className="catalog__btns">
-                  <Pagination
-                    count={initialCount}
-                    page={initialPage}
-                    url={`/spots?category=${initialCategory}&`}
-                  />
-                </div>
-              )}
             </div>
           </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 }
@@ -210,10 +220,5 @@ export async function getServerSideProps({ req, query }) {
     },
   };
 }
-
-
-
-
-
 
 export default SingleSpot;
