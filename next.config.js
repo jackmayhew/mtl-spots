cc9228f048fe99c2ce09fe1970cd166d75262ba0
jack <jacksonmayhew9@gmail.com>
2022-09-28 11:33:33 -0700

    update csp to allow connection to s3 for share page

--- next.config.js
+++ next.config.js
@@ -22,10 +22,10 @@ module.exports = {
       headers: createSecureHeaders({
         contentSecurityPolicy: {
           directives: {
-            defaultSrc: ["'self'", "https://maps.googleapis.com/"],
+            defaultSrc: ["'self'", "https://maps.googleapis.com/", "https://montrealspots.s3.us-east-2.amazonaws.com/"],
             scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://maps.googleapis.com/"],
             styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
-            imgSrc: ["'self'", "https://mtlspots.imgix.net", "data:", "https://ui8-fleet-html.herokuapp.com/", "https://maps.googleapis.com/", "https://maps.gstatic.com/", "https://montrealspots.s3.us-east-2.amazonaws.com/"],
+            imgSrc: ["'self'", "https://mtlspots.imgix.net", "data:", "blob:", "https://ui8-fleet-html.herokuapp.com/", "https://maps.googleapis.com/", "https://maps.gstatic.com/", "https://montrealspots.s3.us-east-2.amazonaws.com/"],
             fontSrc: ["'self'", 'https:', 'data:'],
           },
         },
